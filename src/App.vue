79e1dafcdfa5a62bccf5902ce115fa1bd24a5586
Sunil <sunillangtad54@gmail.com>
2020-03-10 17:55:52 +0530

    Display todolist

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
-    
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-
+import Todos from "./components/Todos";
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false
+        },
+        {
+          id: 4,
+          title: "Todo four",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
