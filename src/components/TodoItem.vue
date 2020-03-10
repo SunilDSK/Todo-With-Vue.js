48f069ef6ab28a408ad3ee513fe43351f965ff7b
Sunil <sunillangtad54@gmail.com>
2020-03-10 18:07:18 +0530

    Passes todo item to TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 0;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: red;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50px;
+        cursor: pointer;
+        float: right;
+    }
+</style>
